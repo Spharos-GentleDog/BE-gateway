1be70a99ad18942116d743198edb7a2bb4bcba2d
daeunk17 <daeunk71@gmail.com>
2023-11-15 15:20:44 +0900

    [fix] log 추가

--- src/main/java/egenius/gateway/filter/AuthorizationVendorHeaderFilter.java
+++ src/main/java/egenius/gateway/filter/AuthorizationVendorHeaderFilter.java
@@ -67,7 +67,8 @@ public class AuthorizationVendorHeaderFilter extends AbstractGatewayFilterFactor
         try {
             // JWT 토큰에서 'role' 정보를 가져온다.
             log.info("토큰 검증");
-            Claims claims = Jwts.parser().setSigningKey(env.getProperty("token.vendorsecret"))
+            log.info("env 확인:{}",env.getProperty("token.secret"));
+            Claims claims = Jwts.parser().setSigningKey(env.getProperty("token.secret"))
                     .parseClaimsJws(jwt).getBody();
             log.info("claims={}",claims);
 
